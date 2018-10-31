71ad7b84525358ea81aea31b2d379e53f051d9ff
Eran Hadar <eran.hadar@gmail.com>
2018-10-31 18:33:41 +0200

    refactor admin operations into discrete classes

--- scalecube-organization/src/main/java/io/scalecube/organization/repository/couchbase/CouchbaseUserOrganizationMembershipRepository.java
+++ scalecube-organization/src/main/java/io/scalecube/organization/repository/couchbase/CouchbaseUserOrganizationMembershipRepository.java
@@ -41,7 +41,7 @@ final class CouchbaseUserOrganizationMembershipRepository
 
   @Override
   public Optional<OrganizationMember> getMember(String userId, Organization organization) {
-    return findById(client(organizati), userId);
+    return findById(client(organization), userId);
   }
 
   private Bucket client(Organization organization) {
