0497324f7e6a125f2981d975665afecf2405f120
aharonha <aharon@om2.com>
2019-01-01 11:21:30 +0200

    revert to develop version

--- scalecube-organization/src/main/java/io/scalecube/tokens/store/PropertiesFileKeyStore.java
+++ scalecube-organization/src/main/java/io/scalecube/tokens/store/PropertiesFileKeyStore.java
@@ -5,49 +5,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Properties;
 
 class PropertiesFileKeyStore implements KeyStore {
-
-  private File parentPath;
-
-  public PropertiesFileKeyStore() {
-    Iterator<String> rootDir = rootPaths.iterator();
-    while (rootDir.hasNext()) {
-      try {
-        File path = new File(rootDir.next());
-        if (!path.isDirectory()) {
-          path.delete();
-          path.mkdirs();
-          if (path.canWrite()) {
-            parentPath = path;
-            return;
-          }
-        }
-      } catch (SecurityException ignoredException) {
-        continue;
-      }
-    }
-  }
 
   @Override
   public void store(String alias, Object key) throws KeyStoreException {
     Properties properties = new Properties();
     try {
-
-      File keystore = new File(parentPath, "keystore.properties");
-      if (keystore.exists()) {
-        properties.load(new FileInputStream(keystore));
+      if (new File("keystore.properties").exists()) {
+        properties.load(new FileInputStream("keystore.properties"));
       }
       properties.put(alias, key);
-      properties.store(new FileOutputStream(keystore), "");
+      properties.store(new FileOutputStream("keystore.properties"), "");
     } catch (IOException ex) {
       throw new KeyStoreException(ex);
     }
   }
-
-  private static final List<String> rootPaths = Arrays.asList("/etc/opt/om2", "/tmp/opt/om2");
 }
