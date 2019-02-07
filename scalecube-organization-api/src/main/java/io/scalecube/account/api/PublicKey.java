3db5f0b78edebd498ffdf7c57976a537ecc1cc40
Dmytro Lazebnyi <dmytro.lazebnyi@jivygroup.com>
2019-02-07 11:43:38 +0200

    Add API to retrieve public key (#89)

---
+++ scalecube-organization-api/src/main/java/io/scalecube/account/api/PublicKey.java
@@ -0,0 +1,47 @@
+package io.scalecube.account.api;
+
+public final class PublicKey {
+
+  private String algorithm;
+  private String format;
+  private byte[] key;
+  private String keyId;
+
+  /**
+   * Only for serialization/deserialization.
+   *
+   * @deprecated for instantiation purposes.
+   */
+  PublicKey() {}
+
+  /**
+   * Creates new instance with information about public key.
+   *
+   * @param algorithm the algorithm used to generate key pair
+   * @param format the format of public key
+   * @param key the encoded key
+   * @param keyId the key identifier
+   */
+  public PublicKey(String algorithm, String format, byte[] key, String keyId) {
+    this.algorithm = algorithm;
+    this.format = format;
+    this.key = key;
+    this.keyId = keyId;
+  }
+
+  public String algorithm() {
+    return algorithm;
+  }
+
+  public String format() {
+    return format;
+  }
+
+  public byte[] key() {
+    return key;
+  }
+
+  public String keyId() {
+    return keyId;
+  }
+}
