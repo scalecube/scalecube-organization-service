ab52eae35324754baf17fb7d88ba48f9119525b4
Eugene M <stingion@gmail.com>
2019-05-02 19:17:40 +0300

    bucket retrieving by 3 times

--- scalecube-organization/src/main/java/io/scalecube/organization/repository/couchbase/CouchbaseRepositoryFactory.java
+++ scalecube-organization/src/main/java/io/scalecube/organization/repository/couchbase/CouchbaseRepositoryFactory.java
@@ -5,10 +5,14 @@ import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
 import io.scalecube.organization.repository.OrganizationsRepository;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class CouchbaseRepositoryFactory {
 
-  private final Bucket bucket;
+  private static final Logger LOGGER = LoggerFactory.getLogger(CouchbaseRepositoryFactory.class);
+
+  private Bucket bucket;
 
   /**
    * Creates a couchbase repository factory, initializes two couchbase clusters.
@@ -20,10 +24,21 @@ public final class CouchbaseRepositoryFactory {
 
     Cluster cluster = nodes.isEmpty() ? CouchbaseCluster.create() : CouchbaseCluster.create(nodes);
 
-    bucket =
-        cluster
+    int time = 0;
+    int maxTimes = 3;
+    do {
+      try {
+        bucket = cluster
             .authenticate(settings.username(), settings.password())
             .openBucket(settings.organizationsBucketName());
+        break;
+      } catch (Exception e) {
+        if (++time == maxTimes) {
+          LOGGER.warn(e.getMessage() + " [times: " + time + "]", e);
+          throw e;
+        }
+      }
+    } while (true);
   }
 
   public OrganizationsRepository organizations() {
