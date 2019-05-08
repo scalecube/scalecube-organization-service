6e8d80d5a01570eac8428364a72d0c6fc9aac727
Eugene M <stingion@gmail.com>
2019-05-08 17:06:18 +0300

    Couchbase cluster timeouts config

--- scalecube-organization/src/main/java/io/scalecube/organization/repository/couchbase/CouchbaseRepositoryFactory.java
+++ scalecube-organization/src/main/java/io/scalecube/organization/repository/couchbase/CouchbaseRepositoryFactory.java
@@ -3,9 +3,12 @@ package io.scalecube.organization.repository.couchbase;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.env.DefaultCouchbaseEnvironment;
+import com.couchbase.client.java.env.DefaultCouchbaseEnvironment.Builder;
 import io.scalecube.organization.repository.OrganizationsRepository;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import reactor.core.publisher.Mono;
 
 public final class CouchbaseRepositoryFactory {
@@ -20,7 +23,16 @@ public final class CouchbaseRepositoryFactory {
   public CouchbaseRepositoryFactory(CouchbaseSettings settings) {
     List<String> nodes = settings.hosts();
 
-    Cluster cluster = nodes.isEmpty() ? CouchbaseCluster.create() : CouchbaseCluster.create(nodes);
+    DefaultCouchbaseEnvironment env = new Builder()
+        .managementTimeout(TimeUnit.SECONDS.toMillis(15))
+        .queryTimeout(TimeUnit.SECONDS.toMillis(15))
+        .viewTimeout(TimeUnit.SECONDS.toMillis(15))
+        .searchTimeout(TimeUnit.SECONDS.toMillis(15))
+        .analyticsTimeout(TimeUnit.SECONDS.toMillis(15))
+        .build();
+
+    Cluster cluster =
+        nodes.isEmpty() ? CouchbaseCluster.create(env) : CouchbaseCluster.create(env, nodes);
 
     bucket = Mono.fromCallable(() ->
         cluster
