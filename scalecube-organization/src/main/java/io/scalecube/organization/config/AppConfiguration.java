824b2c57cffcbfb6f73b5ad3d5649e0d6e34d432
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-10-25 15:36:19 +0300

    Prepared integration with sc-config vault auth via kubernetes

--- scalecube-organization/src/main/java/io/scalecube/organization/config/AppConfiguration.java
+++ scalecube-organization/src/main/java/io/scalecube/organization/config/AppConfiguration.java
@@ -7,13 +7,20 @@ import io.scalecube.config.audit.Slf4JConfigEventListener;
 import io.scalecube.config.source.ClassPathConfigSource;
 import io.scalecube.config.source.SystemEnvironmentConfigSource;
 import io.scalecube.config.source.SystemPropertiesConfigSource;
+import io.scalecube.config.vault.KubernetesVaultTokenSupplier;
 import io.scalecube.config.vault.VaultConfigSource;
+import io.scalecube.config.vault.VaultTokenSupplier;
 import java.nio.file.Path;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 /** Configures the ConfigRegistry with sources. */
 public class AppConfiguration {
+
+  private static final String VAULT_ADDR_PROP_NAME = "VAULT_ADDR";
+  private static final String VAULT_TOKEN_PROP_NAME = "VAULT_TOKEN";
+  private static final String KUBERNETES_VAULT_ROLE_PROP_NAME = "VAULT_ROLE";
+  private static final String VAULT_SECRETS_PATH_PROP_NAME = "VAULT_SECRETS_PATH";
 
   private static final int RELOAD_INTERVAL_SEC = 300;
   private static final Pattern CONFIG_PATTERN = Pattern.compile("(.*)\\.config\\.properties");
@@ -28,9 +35,28 @@ public class AppConfiguration {
             .reloadIntervalSec(RELOAD_INTERVAL_SEC)
             .addListener(new Slf4JConfigEventListener());
 
+    String vaultAddr = System.getenv().get(VAULT_ADDR_PROP_NAME);
+    String secretsPath = System.getenv().get(VAULT_SECRETS_PATH_PROP_NAME);
     // for test purposes without vault access
-    if (System.getenv().get("VAULT_ADDR") != null) {
-      builder.addLastSource("vault", VaultConfigSource.builder().build());
+    if (vaultAddr != null && secretsPath != null) {
+      VaultConfigSource.Builder vaultBuilder = VaultConfigSource.builder();
+
+      String vaultToken = System.getenv().get(VAULT_TOKEN_PROP_NAME);
+      String kubernetesVaultRolePropName = System.getenv().get(KUBERNETES_VAULT_ROLE_PROP_NAME);
+      if (vaultToken == null && kubernetesVaultRolePropName == null) {
+        throw new IllegalArgumentException("Vault auth scheme is required");
+      }
+      if (vaultToken != null && kubernetesVaultRolePropName != null) {
+        throw new IllegalArgumentException("Vault auth scheme is unclear");
+      }
+      if (vaultToken != null) {
+        vaultBuilder.tokenSupplier(new VaultTokenSupplier() {});
+      }
+      if (kubernetesVaultRolePropName != null) {
+        vaultBuilder.tokenSupplier(new KubernetesVaultTokenSupplier());
+      }
+
+      builder.addLastSource("vault", vaultBuilder.secretsPath(secretsPath).build());
     }
     builder.addLastSource("sys_prop", new SystemPropertiesConfigSource());
     builder.addLastSource("env_var", new SystemEnvironmentConfigSource());
